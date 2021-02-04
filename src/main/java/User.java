373b6f0e49eb8f22ab50945eb2c18462e2d44fcc
Janussr <Janusstivang@gmail.com>
2021-02-04 16:34:31 +0100

    user/pass object

---
+++ src/main/java/User.java
@@ -0,0 +1,28 @@
+/**
+ * CREATED BY Janus @ 2021-02-04 - 16:32
+ **/
+public class User {
+    private String userName;
+    private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
